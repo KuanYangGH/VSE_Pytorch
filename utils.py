afdfb6195fce7093f970c5ce7d08cbdb4962f3e1
KuanYangGH <ykxjtuedu@163.com>
2018-06-13 16:45:43 +0800

    utils.py由于pytorch版本的变动带来的更改

--- utils.py
+++ utils.py
@@ -6,7 +6,10 @@ def l2norm(input, p=2.0, dim=1, eps=1e-12):
     """
     Compute L2 norm, row-wise
     """
-    return input / input.norm(p, dim).clamp(min=eps).expand_as(input)
+    # print(input.norm(p, 1).dim())
+    # print(input.norm(p, 1).resize(input.size()[0],1).size())
+    #return input / input.norm(p, dim).clamp(min=eps).expand_as(input)
+    return input / input.norm(p, dim).clamp(min=eps).resize(input.size()[0],1).expand_as(input)
 
 
 def xavier_weight(tensor):
